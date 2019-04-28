b5541009b061644004ea4cd2d33e61cff546815e
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-04-28 10:59:26 +0530

    ExtensionMethod

--- ExtensionMethod/Program.cs
+++ ExtensionMethod/Program.cs
@@ -10,6 +10,24 @@ namespace ExtensionMethod
     {
         static void Main(string[] args)
         {
+            var p = new Person { Name = "John", Age = 33 };
+            var p2 = new Person { Name = "Sally", Age = 35 };
+            p.SayHello(p2);
+
         }
     }
+
+
+    public static class Extension
+    {
+
+        public static void SayHello(this Person person, Person person2)
+        {
+            Console.WriteLine("{0} says Hello {1}", person.Name, person2.Name);
+        }
+
+    }
+
+    
+
 }
